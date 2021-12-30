d8b8bdc411c6e39b7726e05d36ca895b1c5b6323
ryosukeYamazaki <ryamazaki@c-fo.com>
2021-12-30 16:12:55 +0900

    :+1: nextraの使用感を確かめるために色々追加

--- next.config.js
+++ next.config.js
@@ -1,2 +1,7 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+module.exports = withNextra({
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.watchOptions.ignored.push('**/.#*');
+    return config;
+  }
+})
---
+++ pages/posts2/photos2_1.md
@@ -0,0 +1,8 @@
+---
+type: posts
+title: Photos2
+date: 2021-12-30
+---
+
+# posts2
+
